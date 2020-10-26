63bd9678c120a80de8779b7f09884ed1dbd7fad2
Naidence <72426562+Naidence@users.noreply.github.com>
2020-10-27 00:31:35 +0700

    Initial commit

---
+++ app/Http/Controllers/BooksController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Book;
+
+use Laravel\Lumen\Routing\Controller as BaseController;
+
+class BooksController extends BaseController
+{
+    public function index()
+    {
+        return Book::all();
+    }
+}
